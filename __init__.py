dde60cfe66b904c8456ef30913788c371fc6601a
Huynh Chan Luong <mr.omario92@gmail.com>
2026-01-27 12:49:59 +0700

    Move sticky mask crop node to separate file

--- __init__.py
+++ __init__.py
@@ -3,13 +3,16 @@
 
 from .blend_scheduler import DualEndpointColorBlendScheduler
 from .mask_clamped_crop import MaskClampedCrop
+from .mask_clamped_crop_sticky import MaskClampedCropSticky
 
 NODE_CLASS_MAPPINGS = {
     "DualEndpointColorBlendScheduler": DualEndpointColorBlendScheduler,
     "MaskClampedCrop": MaskClampedCrop,
+    "MaskClampedCropSticky": MaskClampedCropSticky,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DualEndpointColorBlendScheduler": "Dual Endpoint Color Blend (by Frames)",
     "MaskClampedCrop": "Mask Tracking Crop (Clamped)",
+    "MaskClampedCropSticky": "Mask Tracking Crop (Sticky)",
 }
