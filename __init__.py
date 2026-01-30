3bb8f7ecb91d86c56c20afae3bcb49e7bfcaa6d9
Huynh Chan Luong <mr.omario92@gmail.com>
2026-01-30 11:24:23 +0700

    Add LTX-2 API text encoding node

--- __init__.py
+++ __init__.py
@@ -2,17 +2,20 @@
 # Gom & đăng ký các node từ ./nodes/*
 
 from .blend_scheduler import DualEndpointColorBlendScheduler
+from .gemma_api_text_encode import GemmaAPITextEncode
 from .mask_clamped_crop import MaskClampedCrop
 from .mask_clamped_crop_sticky import MaskClampedCropSticky
 
 NODE_CLASS_MAPPINGS = {
     "DualEndpointColorBlendScheduler": DualEndpointColorBlendScheduler,
+    "GemmaAPITextEncode": GemmaAPITextEncode,
     "MaskClampedCrop": MaskClampedCrop,
     "MaskClampedCropSticky": MaskClampedCropSticky,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "DualEndpointColorBlendScheduler": "Dual Endpoint Color Blend (by Frames)",
+    "GemmaAPITextEncode": "LTX-2 API Text Encode",
     "MaskClampedCrop": "Mask Tracking Crop (Clamped)",
     "MaskClampedCropSticky": "Mask Tracking Crop (Sticky)",
 }
